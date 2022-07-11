b5e52aaf72cf461465374c15178da210eccdb08c
Gurashish Singh Gill <ggill_be19@thapar.edu>
2022-07-11 13:17:13 +0530

    login/signup new

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
+const fileupload = require("express-fileupload");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(fileupload());
 
-const product = require('./Routes/ProductRoute.js')
-const user = require('./Routes/UserRoute.js')
-const order = require('./Routes/OrderRoute.js')
+const product = require("./Routes/ProductRoute.js");
+const user = require("./Routes/UserRoute.js");
+const order = require("./Routes/OrderRoute.js");
 
-app.use('/api', product);
-app.use('/api', user);
-app.use('/api', order);
+app.use("/api", product);
+app.use("/api", user);
+app.use("/api", order);
 
 module.exports = app;
